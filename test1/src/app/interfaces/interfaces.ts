8dd4a2da6e58d3dfd1d25cff68e45c7f3360f098
OlgaZaglavnova <olga.zaglavnova@mail.ru>
2025-10-31 17:51:00 +0300

    in roles

--- test1/src/app/interfaces/interfaces.ts
+++ test1/src/app/interfaces/interfaces.ts
@@ -1,11 +1,17 @@
 import { MoviesService } from "../services/movies.service";
 
+export interface Role {
+    actorName: string;
+    roleName: string;
+    picture: string;
+}
+
 export interface Movie {
     id: string;
     title: string;
     year: string;
     genre: string;
-    actors: string[];
+    actors: Role[];
     content: string;
     rating: string;
     cover: string;
